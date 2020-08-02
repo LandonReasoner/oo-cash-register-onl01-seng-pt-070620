11da552c9f1ea9c26e288baa6dafade0c27b97f0
Landon Reasoner <reasoner.landon90@gmail.com>
2020-08-02 17:53:10 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -23,7 +23,7 @@ class CashRegister
     if discount == 0
       "There is no discount to apply."
     else 
-      "After the discount, the total comes to $#{."
+      "After the discount, the total comes to $#{@total.total.to_i"
     end
   end
   
